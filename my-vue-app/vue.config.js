2b68b4b55aa42529778be246449bcbb020721b32
Alex A <a.abolmasov@gmail.com>
2024-08-31 15:57:56 +0000

    Добавлены файлы проекта и конфигурации для деплоя на GitHub Pages

--- my-vue-app/vue.config.js
+++ my-vue-app/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
+
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/voicer-vue/'  // Замените 'voicer-vue' на имя вашего репозитория на GitHub
+    : '/',
   devServer: {
     host: '0.0.0.0',           // Accept connections from any IP address
     allowedHosts: 'all',        // Allow requests from any host
@@ -7,4 +11,4 @@ module.exports = {
       webSocketURL: 'auto://0.0.0.0:0/ws',  // Use auto WebSocket URL for hot-reloading
     },
   },
-};
+});
